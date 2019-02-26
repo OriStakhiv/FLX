3e3024fd73149bcd3b22da585f9dd81a004e916b
Ori <orysiastakh@gmail.com>
2019-02-26 23:22:12 +0200

    final version[1]

--- FLX_homework_12/homework/src/app.js
+++ FLX_homework_12/homework/src/app.js
@@ -1,173 +1,208 @@
 
 const rootNode = document.getElementById('root');
-let todoItems = [];
 let zero = 0;
+let k = 1;
 
-function createElement(tag, props, ...children) {
-    const element = document.createElement(tag);
+// main layout
+function renderMainLayout() {
+  clear() // clear document
 
-    Object.keys(props).forEach(key => {
-         element[key] = props[key]
-         } );
-
-    if (children.length > zero) {
-        children.forEach(child => {
-            if (typeof child === 'string') {
-                child = document.createTextNode(child);
-            }
-            element.appendChild(child);
-        });
-    }
-    return element;
+  const mainTitle = createElement('h1', {className: 'main-title'}, 'Simple TODO application');
+  const todoList = createElement('ul', { className: 'todo-list'});
+  const emptyList = createElement('h2', {className: 'todo-empty'}, 'TODO is empty');
+  const mainPage = createElement('div', { className: 'main-page'}, mainTitle, todoList, emptyList);
+  render(mainPage);
+  renderAddButton();
+  renderList();
 }
 
+function renderAddButton() {
+  const addButton = createElement('button', {className: 'add'}, 'Add new task');
 
-function renderLayout(){
-    
-    const mainTitle = createElement('h1', {className: 'main-title'}, 'Simple TODO application');
-    const addButton = createElement('button', {className: 'add'}, 'Add new task');
-    const todoList = createElement('ul', { className: 'todo-list'});
-    const emptyList = createElement('h2', {className: 'todo-empty'}, 'TODO is empty');
-    const mainPage = createElement('div', { className: 'main-page'},mainTitle, addButton, todoList, emptyList);
+  addButton.onclick = () => { 
+    navigateToCreatePage() 
+  };
 
-        addButton.onclick = () => {
-            window.location.hash = '/add_item';
-            clear();
-            rootNode.appendChild(renderAdd(todoItems));
-        }
-       /* if (todoItems.length) {
-            for (let listItem of todoItems) {
-               listItem = createElement('li', {id: listItem.id});
-              const checkbox = createElement('button', {className: listItem.isDone ? 'done' : 'undone'
-              });
-
-              const todoText = createElement('button', {className: 'todo-text',title: 'Click to edit'}, listItem.description);
-              const deleteButton = createElement('button', {className: 'delete'});
-      
-              checkbox.onclick = () => {
-                if (checkbox.className === 'undone') {
-                  checkbox.className = 'done';
-                  lS.setAsDoneById(item.id);
-                  todoList.appendChild(li);
-                }
-             };
-        }
-      todoText.onclick = () => {
-        window.location.hash = `/modify/${item.id}`;
-      };
-    
-      deleteButton.onclick = () => {
-        listItem.delete();
-        lS.removeById(listItem.id);
-      } 
-    }*/
-    
-    return mainPage;
+  render(addButton);
 }
-
-function clear() {
-    rootNode.innerHTML = '';
-}
-
-function renderAdd() {
-    
-    const mainTitle = createElement('h1', {className: 'title'}, 'Add task');
-    const input = createElement('input', {type: 'text', placeholder: 'Task description'});
-    const cancelButton = createElement('button', {className: 'cancel'}, 'Cancel');
-    const saveButton = createElement('button', {className: 'save', disabled: 'true'}, 'Save changes');
-    const mainPage = createElement('div', { className: 'add-page'}, mainTitle, input, cancelButton, saveButton );
-    
-    input.onchange = input.onkeyup = () => {
-      const description = input.value.trim();
-      saveButton.disabled = !description;
-
-      if (event.code === 'Enter' && description) {
-        saveButton.click();
-      }
-    };
-
-    cancelButton.onclick = () => {
-      main();
-    };
-
-    saveButton.onclick = () => {
-      lS(renderAdd(input.value.trim()));
-      main();
-    };
-
-
-return mainPage;
-
-}
-/*
 
 function renderList() {
+  let items = getItems();
 
+  items.forEach((item, id) => {
+    render(prepareListItem(item, id));
+  });
 }
 
-function renderListItem() {
+function renderCreatePage() {
+  clear();
+  const mainTitle = createElement('h1', {className: 'title'}, 'Add task');
+  const input = createElement('input', {type: 'text', placeholder: 'Task description'});
+  const cancelButton = createElement('button', {className: 'cancel'}, 'Cancel');
+  const saveButton = createElement('button', {className: 'save', disabled: 'true'}, 'Save changes');
+  const mainPage = createElement('div', { className: 'add-page'}, mainTitle, input, cancelButton, saveButton );
+  
+  input.onchange = input.onkeyup = () => { // TODO move in separate handler
+    const description = input.value.trim();
 
-}
-*/
-function renderEdit(){
-    const mainPage = this.renderAdd();
-    
-    mainPage.id = 'edit-page';
-    mainPage.querySelector('h1').textContent = 'Modify item';
-    //mainPage.querySelector('input').value = listItem.description;
-    mainPage.querySelector('.save').onclick = () => {
-   // lS(changeDescription(listItem.id, mainPage.querySelector('input').value.trim()));
-      window.location.hash = '/main';
-     
-    };
+    saveButton.disabled = !description;
 
-    return mainPage;
-}
+    if (event.code === 'Enter' && description) {
+      saveButton.click();
+    }
+  };
 
-/*
-function addListItem(){
+  cancelButton.onclick = () => { // TODO move in separate handler
+    renderMainLayout();
+  };
 
-}
-*/
-function lS(description){
-    
-    let id = '';
-    let listItem = {description, id, isDone: false};
-    todoItems.push(listItem);
-    localStorage.setItem('todoItems', JSON.stringify(todoItems));
-    return todoItems;
+  saveButton.onclick = () => { // TODO move in separate handler
+    addItem(input.value.trim());
+    renderMainLayout();
+  };
 
+  render(mainPage);
 }
 
-function lSGetAllItems() {
-    return JSON.parse(localStorage.getItem(todoItems))
+function renderUpdatePage(id) {
+  clear();
+  let item = getItem(id);
+
+  const mainTitle = createElement('h1', {className: 'title'}, 'Edit task');
+  const input = createElement('input', {type: 'text', value: item.description});
+  const cancelButton = createElement('button', {className: 'cancel'}, 'Cancel');
+  const saveButton = createElement('button', {className: 'save', disabled: 'true'}, 'Save changes');
+  const mainPage = createElement('div', { className: 'add-page'}, mainTitle, input, cancelButton, saveButton );
+
+  input.onchange = input.onkeyup = () => { // TODO move in separate handler
+    const description = input.value.trim();
+
+    saveButton.disabled = !description;
+
+    if (event.code === 'Enter' && description) {
+      saveButton.click();
+    }
+  };
+
+  cancelButton.onclick = () => { // TODO move in separate handler
+    renderMainLayout();
+  };
+
+  saveButton.onclick = () => { // TODO move in separate handler
+    updateItem(id, input.value.trim());
+    renderMainLayout();
+  };
+
+  render(mainPage);
+}
+// handlers
+function navigateToCreatePage() {
+  window.location.hash = '/add_item';
+  renderCreatePage();
 }
 
-function lSGetItem(id) {
-    return this.lSGetAllItems().find(item => item.id === id);
+function navigateToUpdatePage(id) {
+  window.location.hash = `/items/${id}`;
+  renderUpdatePage(id);
 }
 
-function lSdoneItem() {
-    return this.lSGetAllItems().filter(item => item.isDone === true);
+function addItem(description) {
+ let items = getItems();
+  items.push({
+    description: description,
+    done: false
+  });
+
+  writeItems(items)
 }
-/*
-function lSUndoneItem(){
 
+function updateItem(id, description) {
+  let items = getItems();
+  items[id].description = description;
+
+  writeItems(items)
 }
 
-
-function destroyListItem(){
-
+function toggleItem(id) {
+  let items = getItems();
+  let item = items.splice(id, 1)[zero];
+  item.done = !item.done;
+  items.push(item);
+  writeItems(items);
+  renderMainLayout();
 }
-*/
+
+function destroyItem(id) {
+  let items = getItems();
+  items.splice(id, 1);
+  writeItems(items);
+  renderMainLayout();
+}
+
+// helpers
+function clear() {
+  rootNode.innerHTML = '';
+}
+
+function getItems() {
+  return JSON.parse(localStorage.getItem('items')) || [];
+}
+
+function getItem(id) {
+  return getItems()[id]
+}
+
+function writeItems(items) {
+  localStorage.setItem('items', JSON.stringify(items));
+}
+
+function render(element) {
+  rootNode.appendChild(element);
+}
+
+function prepareListItem(item, id) {
+  const checkbox = createElement('button', {className: item.done ? 'done' : 'undone'});
+  const label = createElement('label', {className: 'todo-text'}, item.description);
+  const deleteButton = createElement('button', {className: 'delete'});
+
+  checkbox.onclick = () => { 
+    toggleItem(id) 
+  };
+  label.onclick = () => { 
+    navigateToUpdatePage(id) 
+  };
+  deleteButton.onclick = () => {
+    destroyItem(id)
+  };
+
+  return createElement('li',{}, checkbox, label, deleteButton);
+}
+
+function createElement(tag, props, ...children) {
+  const element = document.createElement(tag);
+
+  Object.keys(props).forEach(key => {
+       element[key] = props[key]
+       } );
+
+  if (children.length > zero) {
+      children.forEach(child => {
+          if (typeof child === 'string') {
+              child = document.createTextNode(child);
+          }
+          element.appendChild(child);
+      });
+  }
+  return element;
+}
+// entry point
 function main() {
-    window.history.pushState('', '/', window.location.pathname);
-    window.location.hash = '/main';
+  window.history.pushState('', '/', window.location.pathname);
+  window.location.hash = '/main';
 
-    document.title = 'Main page';
-    clear();
-    rootNode.appendChild(renderLayout(todoItems));
+  document.title = 'Main page';
+  renderMainLayout();
 }
 
-main();
+main(); 
 
